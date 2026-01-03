1123ee09c56b02534e1b5fe4489800f36be0e8c6
Nahun Menem <86641118+NahunMenem@users.noreply.github.com>
2026-01-03 09:38:08 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -19,12 +19,21 @@ import cloudinary.uploader
 # =====================================================
 
 
+import cloudinary
+import cloudinary.uploader
+import os
+
 cloudinary.config(
     cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
     api_key=os.getenv("CLOUDINARY_API_KEY"),
     api_secret=os.getenv("CLOUDINARY_API_SECRET"),
     secure=True
 )
+
+print("☁️ Cloudinary config:",
+      os.getenv("CLOUDINARY_CLOUD_NAME"),
+      os.getenv("CLOUDINARY_API_KEY"))
+
 
 
 app = FastAPI(title="Backend SJ")
